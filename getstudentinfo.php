9d2e57a1cbd372541449e88542e5f179b59793de
chaninkou <michaelkoulove@yahoo.com>
2018-02-09 23:44:23 -0800

    Need to make getstudentinfo work

--- getstudentinfo.php
+++ getstudentinfo.php
@@ -2,7 +2,7 @@
 require_once('../mysqli_connect.php');
 
 $query = "SELECT first_name, last_name, email, street, city, state, zip,
-phone, birth_date, FROM students";
+phone, birth_date FROM students";
 
 
 $response = @mysqli_query($dbc, $query);
